f6d9ce2173a88b93d7e0b4c880fcfe465e4f86bc
ElCholoGamer <ElCholoGamer@users.noreply.github.com>
2022-12-18 18:49:26 +0000

    deploy: d1ee4dff204f4935963bbb82793b66235d608c78

---
+++ _next/static/-wHh8lqNaGLTRq0gG30fx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/280-226c57e40f2283b0.js","static/chunks/pages/index-ef60bdf35ace6008.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
