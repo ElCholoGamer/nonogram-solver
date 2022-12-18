d1ee4dff204f4935963bbb82793b66235d608c78
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-18 13:48:26 -0500

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	assetPrefix: process.env.NODE_ENV === 'production' ? '/nonogram-solver/' : '',
+	assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
